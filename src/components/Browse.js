d6ae4f5882d26711b8b4161efe18e216772f6281
Khyati Patel <khyati.patel@brevitaz.com>
2026-04-12 17:53:50 +0530

    Get Data from TMDB

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,21 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
 
 const Browse = () => {
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      API_OPTIONS,
+    );
+    const json = await data.json();
+    console.log("🚀 ~ getNowPlayingMovies ~ json:", json);
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
   return (
     <div>
       <Header />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,12 @@ export const LOGO =
 
 export const USER_AVATAR =
   "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTBiMjgxYjBiMmRhNzQ2M2Y5YzA1N2M1MzhkNzA1OCIsIm5iZiI6MTc3NTk5NDUwMi43MDgsInN1YiI6IjY5ZGI4Njg2MDIzZDg1ZWYxMjliN2Q5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6Y0Pf0VQ9EM3oIIRQRVxE03bCiuOs03_YzSht9tlPAk",
+  },
+};
