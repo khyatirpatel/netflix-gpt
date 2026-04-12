f93f52649cfc00bd4e161027c5f3342db1434507
Khyati Patel <khyati.patel@brevitaz.com>
2026-04-12 18:30:26 +0530

    Custom Hook for Now Playing Movies

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,20 +1,8 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS,
-    );
-    const json = await data.json();
-    console.log("🚀 ~ getNowPlayingMovies ~ json:", json);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
+  useNowPlayingMovies();
 
   return (
     <div>
